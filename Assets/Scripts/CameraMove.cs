1428d71998aab881cd92bdb93dad4c0661127ee3
SergeyAD <gera-killer2005@rambler.ru>
2021-04-26 18:09:50 +0300

    Lesson1

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject cameraPoint;
+    public GameObject objectPoint;
+
+    public float speed;
+
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, cameraPoint.transform.position, speed * Time.deltaTime);
+        transform.LookAt(objectPoint.transform);
+    }
+
+
+}
---
+++ Assets/Scripts/MainMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMove : MonoBehaviour
+{
+    public GameObject mainObject;
+    public float speedRun;
+    public float speedTurn;
+
+    private Vector3 _vector;
+    private float _turn;
+
+    private void Update()
+    {
+        _vector.z = Input.GetAxis("Vertical");
+        _turn = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        // Движение игрока
+
+        var _move = _vector * speedRun * Time.deltaTime;
+        var turn = _turn * speedTurn * Time.deltaTime;
+
+        
+        mainObject.transform.Translate(_move);
+        mainObject.transform.Rotate(new Vector3(0, turn, 0));
+
+    }
+
+}
